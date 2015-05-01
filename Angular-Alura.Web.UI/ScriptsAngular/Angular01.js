7800c0e613c99154057f9ba45e0e828beb89009b
JeanLLopes <jean.llopes@outlook.com>
2015-05-01 20:13:48 -0300

    Install Bootstrap and Angular, configurar a pagina para o primeira pagina

---
+++ Angular-Alura.Web.UI/ScriptsAngular/Angular01.js
@@ -0,0 +1,10 @@
+﻿
+var app = angular.module("App", []);
+
+app.controller('Angular01Controller', function ($scope) {
+    $scope.total = 0;
+
+    $scope.Contador = function () {
+        $scope.total++;
+    }
+})
