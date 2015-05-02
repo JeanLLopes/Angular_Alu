fb316dca303f94434e86696dc4a406658f5d1a74
JeanLLopes <jean.llopes@outlook.com>
2015-05-02 01:35:08 -0300

    Angular 04

---
+++ Angular-Alura.Web.UI/Pages/Angular04/funcionariosController.js
@@ -0,0 +1,12 @@
+﻿app.controller('FuncionariosController', function($scope) {
+    $scope.funcionarios = [
+        { nome: 'Jose' },
+        { nome: 'Meninao' },
+        { nome: 'Jean' },
+        { nome: 'Maria' }
+        
+    ];
+
+    //PODEMOS SETAR UM VALOR INICIAL PARA O mostrar
+    $scope.mostrar = false;
+});
---
+++ Angular-Alura.Web.UI/Pages/Angular04/main.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('Funcionarios', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'partials/Bem-Vindo.tpl.html'
+    });
+
+    $routeProvider.when('/Funcionarios', {
+        templateUrl: 'partials/Funcionarios.tpl.html',
+        controller: 'FuncionariosController'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/' });
+})
