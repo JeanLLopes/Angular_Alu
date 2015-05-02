ff0abbb6397a460c26c17127c37b419c7edbc99e
JeanLLopes <jean.llopes@outlook.com>
2015-05-02 00:43:45 -0300

    Angular03

---
+++ Angular-Alura.Web.UI/Pages/Angular03/main.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('Funcionarios', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'partials/Bem-Vindo.tpl.html'
+    });
+
+    $routeProvider.when('/Funcionarios', {
+        templateUrl: 'partials/Funcionarios.tpl.html'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/' });
+})
